2ba852bcfd74fa5dfd4e860b9bbf4d4b7cba531b
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-26 10:58:22 -0500

    installed chalk dependency

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-
+const chalk = require('chalk');
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 server.listen(port, () =>
-  console.log(`\n** Tipsease running on http://localhost:${port} **\n`),
+  console.log(
+    chalk.blue.bold(`\n** Tipsease running on http://localhost:${port} **\n`),
+  ),
 );
