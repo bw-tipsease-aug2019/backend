1696fc9b8492b7e5975dbc777381ee3eae592d06
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-26 11:00:03 -0500

    setup db config file

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,3 +4,8 @@ const config = require('../knexfile.js');
 const environment = process.env.DB_ENV || 'development';
 
 module.exports = knex(config[environment]);
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+
+module.exports = knex(knexConfig.development);
